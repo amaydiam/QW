a444c93f6aeae5b2fa5ec30e72f9021306abf5f1
binderbyte <binderbyte@gmail.com>
2017-01-11 13:50:45 +0700

    add validator login and register

--- app/src/main/java/com/qwash/washer/ui/activity/LoginUserActivity.java
+++ app/src/main/java/com/qwash/washer/ui/activity/LoginUserActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,9 +18,16 @@ import com.joanzapata.iconify.fonts.MaterialCommunityModule;
 import com.joanzapata.iconify.fonts.MaterialIcons;
 import com.joanzapata.iconify.fonts.MaterialModule;
 import com.joanzapata.iconify.fonts.SimpleLineIconsModule;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.qwash.washer.R;
 import com.qwash.washer.ui.widget.RobotoRegularButton;
 import com.qwash.washer.ui.widget.RobotoRegularEditText;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,11 +37,19 @@ public class LoginUserActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @NotEmpty
+    @Length(min = 5, max = 100, trim = true, messageResId = R.string.val_email_length)
+    @Email
     @BindView(R.id.email)
     RobotoRegularEditText email;
+
+    @NotEmpty
+    @Length(min = 4, max = 10, trim = true, messageResId = R.string.val_password_length)
     @BindView(R.id.password)
     RobotoRegularEditText password;
 
+    private Validator validator;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         Iconify
@@ -45,6 +61,7 @@ public class LoginUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_user);
         ButterKnife.bind(this);
+        setValidator();
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(
                 new IconDrawable(this, MaterialIcons.md_arrow_back)
@@ -60,9 +77,42 @@ public class LoginUserActivity extends AppCompatActivity {
 
     }
 
+    private void setValidator() {
+        validator = new Validator(this);
+        validator.setValidationListener(new Validator.ValidationListener() {
+            @Override
+            public void onValidationSucceeded() {
+                loginSucces();
+            }
+
+            @Override
+            public void onValidationFailed(List<ValidationError> errors) {
+                loginFailed(errors);
+            }
+        });
+    }
+
+    private void loginSucces() {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    private void loginFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getApplicationContext());
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
     @OnClick(R.id.btn_login)
     void Login() {
-        startActivity(new Intent(this, HomeActivity.class));
+        validator.validate();
     }
 
     @OnClick(R.id.btn_forgot_password)
